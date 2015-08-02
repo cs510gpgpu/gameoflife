891d012c738a85204ebf87e80068655273e67909
Sean <mahan2@pdx.edu>
2015-08-01 17:53:57 -0700

    updated test script, basic testing supported

--- src/test.py
+++ src/test.py
@@ -1,10 +1,17 @@
 import os
 import platform
+import subprocess
 
 platformWinOrNix = platform.system()
 
 
 if platformWinOrNix ==  'Windows':
     print("You are running Windows")
+    # basic output
+    run1 = subprocess.Popen(["nvprof", "--log-file", "test1.txt", "--csv", "--timeout", "30", "..\\Release\\gameoflife.exe", "1024", "768", "OpenGL"], stdout=subprocess.PIPE)
+    run1.stdout.readlines()
+    # visual profiler output                                                                                                    #these can be put in a loop and run at many variations
+    run2 = subprocess.Popen(["nvprof", "--analysis-metrics", "-o", "run1.nvprof", "--timeout", "30", "..\\Release\\gameoflife.exe", "1024", "768", "OpenGL"], stdout=subprocess.PIPE)
+    run3.stdout.readlines()
 else:
     print("You are running Linux")
