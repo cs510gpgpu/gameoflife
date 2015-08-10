0fa4478f97ed6f04ee74aacc10133241007b9df3
Mitch Souders <mitch.souders@gmail.com>
2015-08-10 14:54:21 -0700

    Remove cruft from GOL
    Add flames to test.json

--- src/gol.cu
+++ src/gol.cu
@@ -10,6 +10,7 @@
 #include <time.h> // for nanosleep
 #include "common.h"
 #include "nv/gpu_anim.h"
+#include "nv/cpu_anim.h"
 #include <assert.h>
 
 #ifdef _WIN32
@@ -20,12 +21,6 @@
 #ifndef TILE_WIDTH
 #define TILE_WIDTH 32
 #endif
-
-const int offsets[8][2] = {{-1, 1},{0, 1},{1, 1},
-                           {-1, 0},       {1, 0},
-                           {-1,-1},{0,-1},{1,-1}};
-
-__constant__ int cuda_offsets[8][2];
 
 struct Args_t globalArgs;
 
@@ -57,65 +52,6 @@ void swap_board(int ** b1, int ** b2)
     int * tmp = *b1;
     *b1 = *b2;
     *b2 = tmp;
-}
-
-void step(int * next, int * current, int WIDTH, int HEIGHT) {
-    // coordinates of the cell we're currently evaluating
-    int x, y;
-    // offset index, neighbor coordinates, alive neighbor count
-    int i, nx, ny, num_neighbors;
-
-    // write the next board state
-    for (y=0; y<HEIGHT; y++) {
-        for (x=0; x<WIDTH; x++) {
-            // count this cell's alive neighbors
-            num_neighbors = 0;
-            for (i=0; i<8; i++) {
-                // To make the board torroidal, we use modular arithmetic to
-                // wrap neighbor coordinates around to the other side of the
-                // board if they fall off.
-                nx = (x + offsets[i][0] + WIDTH) % WIDTH;
-                ny = (y + offsets[i][1] + HEIGHT) % HEIGHT;
-                if (current[ny * WIDTH + nx]) {
-                    num_neighbors++;
-                }
-            }
-
-            // apply the Game of Life rules to this cell
-            next[y * WIDTH + x] = 0;
-            if ((current[y * WIDTH + x] && num_neighbors==2) ||
-                    num_neighbors==3) {
-                next[y * WIDTH + x] = 1;
-            }
-        }
-    }
-}
-
-/* the naive approach is more or less a copy/pasted version of the original algorithm
-   which has minor changes to work within a kernel space */
-__global__ void naive_compute_gol(int * next, int * board, int WIDTH, int HEIGHT)
-{    
-    int x = (blockIdx.x * blockDim.x) + threadIdx.x;
-    int y = (blockIdx.y * blockDim.y) + threadIdx.y;
-
-    if (x >= WIDTH || y >= HEIGHT) {
-        return;
-    }
-    
-    int num_neighbors = 0;
-    for (int i=0; i<8; i++) {
-        int nx = (x + cuda_offsets[i][0] + WIDTH) % WIDTH;
-        int ny = (y + cuda_offsets[i][1] + HEIGHT) % HEIGHT;
-        if (board[ny * WIDTH + nx]) {
-            num_neighbors++;
-        }
-    }
-    
-    next[y * WIDTH + x] = 0;
-    if ((board[y * WIDTH + x] && num_neighbors==2) ||
-        num_neighbors==3) {
-        next[y * WIDTH + x] = 1;
-    }
 }
 
 __global__ void tile_compute_gol_bitmap(uchar4* bitmap, int * next, int * board, int WIDTH, int HEIGHT)
@@ -189,111 +125,25 @@ void copy_board(int * target, int * src, int len)
     memcpy(target, src, len * sizeof(int));
 }
 
-/* the naive game of life, which uses constant memory for offsets */
-void gol_naive_device(int * board, int iterations, int WIDTH, int HEIGHT, int block_width)
-{
-    int * dev_board;
-    int * dev_next;
-    int i;
-    gpuErrchk(cudaMalloc((void **) &dev_board, sizeof(int) * WIDTH * HEIGHT));
-    gpuErrchk(cudaMalloc((void **) &dev_next, sizeof(int) * WIDTH * HEIGHT));
-    
-    gpuErrchk(cudaMemcpy(dev_board, board, sizeof(int) * WIDTH * HEIGHT, cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpyToSymbol(cuda_offsets, offsets, sizeof(cuda_offsets) ));
-
-    dim3 threadsPerBlock(block_width, block_width);
-    dim3 numBlocks(ceil((float)WIDTH / block_width),ceil((float)HEIGHT / block_width));
-
-    gpuErrchk( cudaDeviceSynchronize() ); /* wait for mem to be copied? */
-    for (i = 0; i < iterations; i++) {
-        tile_compute_gol_bitmap<<<numBlocks, threadsPerBlock>>>(NULL, dev_next, dev_board, WIDTH, HEIGHT);
-        gpuErrchk( cudaPeekAtLastError() );
-        gpuErrchk( cudaDeviceSynchronize() ); /* wait for computation to complete */
-
-        /* swap the two boards to allow memory to already be in the correct location */
-        swap_board(&dev_next, &dev_board);
-    }
-
-    /* only copy memory from device when the user program wants it (after the iterations) */
-    gpuErrchk(cudaMemcpy(board, dev_board, sizeof(int) * WIDTH * HEIGHT, cudaMemcpyDeviceToHost));
-
-    gpuErrchk(cudaFree(dev_board));
-    gpuErrchk(cudaFree(dev_next));
-}
-
-/* the tiled game of life management function */
-void gol_device(int * board, int iterations, int WIDTH, int HEIGHT, int block_width)
-{
-    int * dev_board;
-    int * dev_next;
-    int i;
-    gpuErrchk(cudaMalloc((void **) &dev_board, sizeof(int) * WIDTH * HEIGHT));
-    gpuErrchk(cudaMalloc((void **) &dev_next, sizeof(int) * WIDTH * HEIGHT));
-    
-    gpuErrchk(cudaMemcpy(dev_board, board, sizeof(int) * WIDTH * HEIGHT, cudaMemcpyHostToDevice));
-    /* this will "over" allocate threads, to fully populate shared memory on each block */
-    dim3 threadsPerBlock(block_width, block_width);
-    dim3 numBlocks(ceil((float)WIDTH / (block_width - 2)),ceil((float)HEIGHT / (block_width - 2)));
-
-    gpuErrchk( cudaDeviceSynchronize() ); /* wait for mem to be copied? */
-    for (i = 0; i < iterations; i++) {
-        tile_compute_gol_bitmap<<<numBlocks, threadsPerBlock>>>(NULL, dev_next, dev_board, WIDTH, HEIGHT);
-        gpuErrchk( cudaPeekAtLastError() );
-        gpuErrchk( cudaDeviceSynchronize() ); /* wait for computation to complete */
-
-        /* swap the two boards to allow memory to already be in the correct location */
-        swap_board(&dev_next, &dev_board);
-    }
-
-    /* only copy memory from device when the user program wants it (after the iterations) */
-    gpuErrchk(cudaMemcpy(board, dev_board, sizeof(int) * WIDTH * HEIGHT, cudaMemcpyDeviceToHost));
-
-    gpuErrchk(cudaFree(dev_board));
-    gpuErrchk(cudaFree(dev_next));
-}
-
-void animate(int * board, int WIDTH, int HEIGHT, int block_width, int do_delay, int profile) {
-	#ifndef _WIN32
-	struct timespec delay = {0, 125000000}; // 0.125 seconds
-    struct timespec remaining;
-	#endif
-	
-	int iteration = 0;
-    while (1) {
-        iteration++;
-        if (!profile) {
-            printf("Iteration: %d\n", iteration);
-            print_board(board, WIDTH, HEIGHT);
-        }
-        gol_device(board, 1, WIDTH, HEIGHT, block_width);
-        // We sleep only because textual output is slow and the console needs
-        // time to catch up. We don't sleep in the graphical X11 version.
-        if (do_delay) {
-#ifdef _WIN32
-            std::this_thread::sleep_for(std::chrono::milliseconds(125));
-#else
-            nanosleep(&delay, &remaining);
-#endif
-        }
-        timeout(&globalArgs, iteration);
-    }
-}
-
-struct DataBlock {
+struct GPUDataBlock {
     int HEIGHT;
     int WIDTH;
+    int * board;
     int * dev_board;
     int * dev_next;
-    cudaEvent_t start, stop;
-    int frames;
-    float totalTime;
     int profile;
     int block_width;
 };
 
+struct CPUDataBlock {
+    uchar4 * dev_bitmap;
+    GPUDataBlock gpu;
+    CPUAnimBitmap * bitmap;
+};
 
-void anim_gpu( uchar4* outputBitmap, DataBlock *d, int ticks ) {
-    gpuErrchk( cudaEventRecord( d->start, 0 ) );
+
+void generate_frame_gpu( uchar4* outputBitmap, GPUDataBlock *d, int ticks ) {
+    static int iterations = 0;
     dim3 threadsPerBlock(d->block_width, d->block_width);
     dim3 numBlocks(ceil((float)d->WIDTH / (d->block_width - 2)),ceil((float)d->HEIGHT / (d->block_width - 2)));
     assert(d->block_width == TILE_WIDTH);
@@ -301,125 +151,96 @@ void anim_gpu( uchar4* outputBitmap, DataBlock *d, int ticks ) {
     tile_compute_gol_bitmap<<<numBlocks, threadsPerBlock>>>(outputBitmap, d->dev_next, d->dev_board, d->WIDTH, d->HEIGHT);
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaDeviceSynchronize() ); /* wait for computation to complete */
-    gpuErrchk( cudaEventRecord( d->stop, 0 ) );
-    gpuErrchk( cudaEventSynchronize( d->stop ) );
-    float   elapsedTime;
-    gpuErrchk( cudaEventElapsedTime( &elapsedTime, d->start, d->stop ) );
+
     /* swap the two boards to allow memory to already be in the correct location */
     swap_board(&d->dev_next, &d->dev_board);
 
-    d->totalTime += elapsedTime;
-    d->frames++;
-    if ((d->frames & 0x7F) == 0 && !d->profile) {
-        printf( "(%d) Average Time per frame:  %3.1f ms\n",
-            d->frames, d->totalTime/d->frames );
-    }
-    timeout(&globalArgs, d->frames);
+    timeout(&globalArgs, iterations++);
 }
 
-void anim_exit( DataBlock *d ) {
+void generate_frame_cpu(CPUDataBlock *d, int ticks )
+{
+    generate_frame_gpu(d->dev_bitmap, &d->gpu, ticks);
+    gpuErrchk(cudaMemcpy(d->bitmap->get_ptr(), d->dev_bitmap, d->bitmap->image_size(), cudaMemcpyDeviceToHost));
+}
+
+void cleanup_gpu(GPUDataBlock * d)
+{
+    free(d->board);
     gpuErrchk(cudaFree(d->dev_board));
     gpuErrchk(cudaFree(d->dev_next));
-    gpuErrchk( cudaEventDestroy( d->start ) );
-    gpuErrchk( cudaEventDestroy( d->stop ) );
 }
 
-void gpu_gameoflife(int WIDTH, int HEIGHT, int block_width, int * board, int profile)
+void cleanup_cpu(CPUDataBlock *d)
 {
-    DataBlock   d;
-    GPUAnimBitmap bitmap( WIDTH, HEIGHT, &d );
+    cleanup_gpu(&d->gpu);
+    gpuErrchk(cudaFree(d->dev_bitmap));
+}
 
-    d.HEIGHT = HEIGHT;
-    d.WIDTH = WIDTH;
-    d.frames = 0;
-    d.totalTime = 0;
-    d.profile = profile;
-    d.block_width = block_width;
-    
-    gpuErrchk( cudaEventCreate( &d.start ) );
-    gpuErrchk( cudaEventCreate( &d.stop ) );
-    
-    gpuErrchk(cudaMalloc((void **) &d.dev_board, sizeof(int) * WIDTH * HEIGHT));
-    gpuErrchk(cudaMalloc((void **) &d.dev_next, sizeof(int) * WIDTH * HEIGHT));
-    
-    gpuErrchk(cudaMemcpy(d.dev_board, board, sizeof(int) * WIDTH * HEIGHT, cudaMemcpyHostToDevice));
+void gpu_exit( GPUDataBlock *d )
+{
+    cleanup_gpu(d);
+}
 
+void cpu_exit(CPUDataBlock *d)
+{
+    cleanup_cpu(d);
+}
+
+void init_gpu_datablock(GPUDataBlock * d)
+{    
+    d->HEIGHT = globalArgs.height;
+    d->WIDTH = globalArgs.width;
+    d->profile = globalArgs.profile;
+    d->block_width = globalArgs.blockwidth;
+    d->board = (int *)malloc(sizeof(int) * d->HEIGHT * d->WIDTH);
+    
+    fill_board(d->board, d->HEIGHT * d->WIDTH);
+    
+    gpuErrchk(cudaMalloc((void **) &d->dev_board, sizeof(int) * d->WIDTH * d->HEIGHT));
+    gpuErrchk(cudaMalloc((void **) &d->dev_next, sizeof(int) * d->WIDTH * d->HEIGHT));
+  
+    gpuErrchk(cudaMemcpy(d->dev_board, d->board, sizeof(int) * d->WIDTH * d->HEIGHT, cudaMemcpyHostToDevice));
+    
     gpuErrchk( cudaDeviceSynchronize() ); /* wait for mem to be copied */
-    
-    bitmap.anim_and_exit( (void (*)(uchar4*,void*,int))anim_gpu,
-        (void (*)(void*))anim_exit );
 }
 
-void plain_old(int WIDTH, int HEIGHT, int block_width, int * default_board, int * default_next, int * cuda_board, int profile)
+void init_cpu_datablock(CPUDataBlock * d)
 {
-    int elements = WIDTH * HEIGHT;
-    // Sanity Check CUDA for 10 Steps (each checked)
-    for (int i = 0; i < 10; i++) {
-        step(default_next, default_board, WIDTH, HEIGHT);
-        gol_device(cuda_board, 1, WIDTH, HEIGHT, block_width);
-        
-        if (!compare_board(cuda_board, default_next, elements)) {
-            print_board(cuda_board, WIDTH, HEIGHT);
-            print_board(default_next, WIDTH, HEIGHT);
-            printf("SANITY: Boards do not match (iteration %d)!\n", i);
-            exit(1);
-        }
-
-        swap_board(&default_next, &default_board);
-    }
-
-    //Sanity Check CUDA with 10 unmonitored (completely on device) steps
-    int unmonitored = 10;
-    for (int i = 0; i < unmonitored; i++) {
-        step(default_next, default_board, WIDTH, HEIGHT);
-        swap_board(&default_next, &default_board);
-    }
-    gol_device(cuda_board, unmonitored, WIDTH, HEIGHT, block_width);
-    if (!compare_board(cuda_board, default_board, elements)) {
-        print_board(cuda_board, WIDTH, HEIGHT);
-        print_board(default_board, WIDTH, HEIGHT);
-        printf("SANITY: Boards do not match (after 10 on device iterations)!\n");
-        exit(1);
-    }
-
-    /* it appears to be sane, run the animation routine */
-    animate(cuda_board, WIDTH, HEIGHT, block_width, 1, profile);
+    init_gpu_datablock(&d->gpu);
 }
-
 
 int main(int argc, char *argv[]) {
     processArgs("gol", argv, argc, &globalArgs);
-    
-	int elements = globalArgs.width * globalArgs.height;
-    
-    int * default_board = (int *)malloc(sizeof(int) * elements);
-    int * default_next = (int *)malloc(sizeof(int) * elements);
-
-    int * cuda_board = (int *)malloc(sizeof(int) * elements);
     srand(time(NULL));
  
-    fill_board(default_board, elements);
-    copy_board(cuda_board, default_board, elements);
-
     switch(globalArgs.mode) {
     case PROFILE_NONE:
-        plain_old(globalArgs.width, globalArgs.height, globalArgs.blockwidth, default_board, default_next, cuda_board, globalArgs.profile);
+        printf("Profile None is unimplemented\n");
         break;
     case PROFILE_GPU:
-        gpu_gameoflife(globalArgs.width, globalArgs.height, globalArgs.blockwidth, cuda_board, globalArgs.profile);
+        {
+            GPUDataBlock   d;
+            GPUAnimBitmap bitmap( globalArgs.width, globalArgs.height, &d );
+            init_gpu_datablock(&d);    
+            bitmap.anim_and_exit( (void (*)(uchar4*,void*,int))generate_frame_gpu,
+                (void (*)(void*))cleanup_gpu );
+        }
         break;
     case PROFILE_CPU:
-        animate(cuda_board, globalArgs.width, globalArgs.height, globalArgs.blockwidth, 0, globalArgs.profile);
+        {
+            CPUDataBlock data;
+            init_gpu_datablock(&data.gpu);
+            CPUAnimBitmap bitmap(data.gpu.WIDTH, data.gpu.HEIGHT, &data);
+            data.bitmap = &bitmap;
+            gpuErrchk(cudaMalloc((void**)&data.dev_bitmap, bitmap.image_size()));
+            bitmap.anim_and_exit((void (*)(void*,int))generate_frame_cpu, (void(*)(void*))cleanup_cpu);
+        }
         break;
     default:
         printf("Unhandled mode by game of life.\n");
         exit(1);
     }
-
-    /* cleanup no longer needed boards */
-    free(default_board);
-    free(default_next);   
-    free(cuda_board);
 
     return 0;
 }
